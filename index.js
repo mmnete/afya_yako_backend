5dd99fa89a542b3770cf7342769e01df53f010f2
mmnete <mnetemohamed@gmail.com>
2023-12-14 21:49:23 +0000

    Added cors support to get requests from FE

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import {PORT} from './config.js';
 import express from 'express';
+import cors from 'cors';
 import router from './src/routes/routes.js';
 
 const app = express();
 app.use(express.json()); // Allow parsing of json payloads.
+app.use(cors()); // Allows FE to send requests.
 
 app.use(router);
 
